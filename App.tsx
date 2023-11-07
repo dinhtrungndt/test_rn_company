2160b0f06636635a085244af44688976cb4bc72c
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2023-11-06 18:22:23 -0600

    Up start

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Text } from "react-native";
+import React, {useEffect} from 'react';
+import HomeScreen from './src/home';
 
-const App =() =>{
-  return(
-    <Text>Nguyễn Đình Trưng</Text>
-  )
-}
+const App = () => {
+  return (
+    <HomeScreen/>
+  );
+};
 
 export default App;
